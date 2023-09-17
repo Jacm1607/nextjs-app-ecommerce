ac19c0759e2244fb766381f631a6f9a8a73eceef
Jose <chavarriaj84@gmail.com>
2023-09-17 12:02:22 -0400

    add page terminos y condiciones

---
+++ app/terminos-condiciones/page.tsx
@@ -0,0 +1,23 @@
+export default function TermsConditions() {
+
+  return (
+    <div className="mx-60">
+      <p className="my-16"><span className="  font-bold text-3xl text-sky-900">Términos y Condiciones</span></p>
+      <p className="my-4">Hauscenter a través de nuestra pagina web www.hauscenter.com.bo , comunica a nuestros Clientes los términos y condiciones que van a regir a los efectos de la utilización por éstos del Sitio.</p>
+      <p className="my-4">Se informa en el presente la política de protección de datos de carácter personal con el objeto que los Clientes Usuarios determinen en forma voluntaria si desean dar a HAUSCENTER sus datos personales en ocasión del uso del Sitio o compra.</p>
+
+      <p className="my-4">Presentamos los términos y condiciones a los efectos del uso por parte del Usuario Cliente del sitio www.hauscenter.com.bo</p>
+
+      <p className="my-4">El sólo uso del Sitio implica la aceptación irrevocable e incondicional por parte del Usuario/Cliente de todos los términos y condiciones fijados en el presente, asumiendo la obligación de respetar las mismas.</p>
+
+      <p className="my-4">HAUSCENTER se reserva el derecho de realizar cambios en el Sitio, en las condiciones de uso y en las notificaciones que correspondieren, en cualquier momento, y con la simple modificación del presente, comunicándolo a través de publicación en el Sitio.</p>
+      <p className="my-4">Asimismo, Hauscenter se reserva el derecho en cualquier momento de interrumpir el servicio ofrecido, sin previo aviso a los Usuarios/Clientes, en forma transitoria o definitiva, y sin necesitar en ningún momento consentimiento del Usuario/Cliente.</p>
+
+      <p className="my-4">En función de lo expresado en el punto 2 del presente, cada vez que el Usuario /Cliente desee utilizar el Sitio, deberá aceptar a priori todos y cada uno de los términos y condiciones especificados en el presente.</p>
+
+      <p className="my-4">Protección de los Datos Personales: Hauscenter tiene cuidado de todos los procesos requeridos a los efectos de Protección de los Datos Personales de los Usuarios/Clientes, con el objeto de evitar el mal uso, alteración, acceso no autorizado y/o robo de los Datos Personales suministrados por el Usuario/Cliente a Hauscenter.</p>
+      <p className="my-4">Sin perjuicio de lo expresado en el presente, el Usuario/Cliente acepta y reconoce que las medidas de seguridad en Internet no son inviolables. El Usuario/Cliente que suministre sus datos personales tendrá la posibilidad de ejercer su derecho de acceso a los mismos. También podrá rectificar los mismos, o solicitar el retiro de su nombre de la base de datos.</p>
+      <p className="my-4 mb-16">Toda información suministrada será tratada por Hauscenter conforme términos previstos en la Ley N° 164 – Ley General de Telecomunicaciones.</p>
+    </div>
+  )
+}
