7d5813f27fa78489744d226012671d31d1de4881
Jose <chavarriaj84@gmail.com>
2023-09-17 13:13:19 -0400

    add config images url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['www.dashboard.hauscenter.com.bo']
+    },
+}
 
 module.exports = nextConfig
