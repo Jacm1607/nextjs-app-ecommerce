f905e90b1e51816c85f6de99491abb942d3e4699
Jose <chavarriaj84@gmail.com>
2023-09-28 16:41:13 -0400

    [FIX] component product card show offer price

--- components/ui/cardProduct.tsx
+++ components/ui/cardProduct.tsx
@@ -3,7 +3,7 @@ import Img from "./img";
 import { lenghtText } from "@/lib/string";
 import { validateOffer } from "@/lib/helpers";
 
-const CardProduct = ({product}:any) => {
+const CardProduct = ({ product }: any) => {
     return <Link href={`/producto/${product.attributes.slug}`} className="relative flex flex-col content-between border-2 border-primary rounded-[50px] h-[342px] w-[290px]">
         {
             validateOffer(product.attributes.precio, product.attributes.precio_oferta, product.attributes.inicio_oferta, product.attributes.limite_oferta) ? <div className="absolute"><Img className=" rounded-tl-[49px]" baseUrl={false} url={'/oferta.png'} width={"80%"} height={"80%"} objectFit={"cover"} /></div> : <></>
@@ -13,7 +13,16 @@ const CardProduct = ({product}:any) => {
         </div>
         <div className="mt-auto mb-6">
             <p className="text-primary font-light text-center">{lenghtText(product.attributes.nombre, 60)}</p>
-            <p className="font-extrabold text-primary text-center">Bs {product.attributes.precio}</p>
+            {
+                validateOffer(product.attributes.precio, product.attributes.precio_oferta, product.attributes.inicio_oferta, product.attributes.limite_oferta)
+                    ?
+                    <>
+                        <p className="text-primary text-center line-through decoration-red-500 decoration-2">Bs {product.attributes.precio}</p>
+                        <p className="font-extrabold text-primary text-center">Bs {product.attributes.precio_oferta}</p>
+                    </>
+                    :
+                    <p className="font-extrabold text-primary text-center">Bs {product.attributes.precio}</p>
+            }
         </div>
     </Link>
 }
