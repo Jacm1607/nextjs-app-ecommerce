cdf93e1f16ef0792bf9713685b98674fad6a226a
Jose <chavarriaj84@gmail.com>
2023-09-29 10:29:38 -0400

    [UPDATE] component brandHome

--- components/page/brandHome.tsx
+++ components/page/brandHome.tsx
@@ -8,12 +8,18 @@ const BranHome = () => {
             <div className="col-span-5">
                 <TitleBorder title={"mARCAS RECONOCIDAS"} />
             </div>
-            <div className="col-span-4">
-                <div className="flex w-full overflow-x-auto space-x-8 py-8 pl-10">
+            <div className="col-span-5">
+                <div className=" columns-5 space-x-8 space-y-4">
                     <Image src={'/brands/marca1.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
                     <Image src={'/brands/marca2.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
                     <Image src={'/brands/marca3.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
                     <Image src={'/brands/marca4.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca5.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca6.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca7.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca8.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca9.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
+                    <Image src={'/brands/marca10.png'} alt="Marca 1" width={620} height={230} style={{width: '200px', height: '100px', objectFit: 'contain'}}></Image>
                 </div>
             </div>
         </div>
